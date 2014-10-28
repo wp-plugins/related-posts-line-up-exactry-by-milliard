22d8e85452fb311ca1bd00e096375596cb624a55
shisuh <shisuh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-28 23:00:35 +0000

    add includes
    git-svn-id: https://plugins.svn.wordpress.org/related-posts-line-up-exactry-by-milliard/trunk@1015521 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/admin_setting.php
+++ includes/admin_setting.php
@@ -11,6 +11,8 @@
 	}
 	$show_bottom = get_option("SS_RP_SHOW_BOTTOM");
 	$show_insert = get_option("SS_RP_SHOW_INSERT");
+	$header_text = get_option("SS_RP_HEADER_TEXT"); 
+	$footer_text_color = get_option("SS_RP_FOOTER_TEXT_COLOR"); 
 ?>
 <div class="wrap">
       <div id="icon-options-general" class="icon32"><br></div>
@@ -38,10 +40,37 @@
 					</div>
 				</td>
 			</tr>
+			<tr>
+				<th scope="row"><label for="blogname">紹介文(HTML不可)</label></th>
+				<td>
+					<div class="field">
+						<input type="text" placeholder="何も設定しない場合、こんな記事も書いてますが表示されます" value="<?php echo $header_text; ?>" style="width:90%" name="headerText"/>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row">ツール名文字色</th>
+				<td>
+					<div class="field">
+						<input type="text" placeholder="例：#6e6e6e" value="<?php echo $footer_text_color; ?>"  name="footerTextColor"/>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<th colspan="2">
+					<input type="submit" name="update_options" class="button-primary" value="<?php _e('Save Changes'); ?>">
+				</th>
+			</tr>
+			<tr>
+				<th colspan="2">
+					<div><a target="_blank" href="http://corp.shisuh.com/?p=497">よくあるカスタマイズ方法はこちらを参照下さい</a></div>
+				</th>
+			</tr>
+
+
 		</tbody>
 	</table>
 
-	<input type="submit" name="update_options" class="button-primary" value="<?php _e('Save Changes'); ?>">
 	</form>
 </div>
 <?php ?>

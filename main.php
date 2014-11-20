dbc622b5b6d646b1c6c51b7b11379ed07f6f8cb6
shisuh <shisuh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-20 07:54:00 +0000

    fix rss redirect error
    git-svn-id: https://plugins.svn.wordpress.org/related-posts-line-up-exactry-by-milliard/tags/0.0.6@1029219 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- main.php
+++ main.php
@@ -1,31 +1,93 @@
 <?php
 
 /*
-Plugin Name: シスウ関連ページ
+Plugin Name: Milliard Related Page
 Description: Related Post Plugin which insert and line extactly related posts bottom of <body> and content. 
 Author: Shisuh.inc
-Version: 0.0.2
+Version: 0.0.6
 */
 class ShisuhRelatedPage { 
 
 	function __construct() {
 		define("SS_RP_PLUGIN_DIR",dirname(__FILE__));
 		$this->host = defined("SS_HOST") ? SS_HOST : "www.shisuh.com";
-		add_filter('template', array($this, 'template'), 0);
-		add_filter('request', array($this, 'filter_request'), 1 );
 		if ( is_admin() ) {
-
+			$match = preg_match("/plugins.php/i",$_SERVER["PHP_SELF"]);
+			$rp_init = get_option("SS_RP_INIT");
+			$rp_confirm = get_option("SS_RP_CONFIRM");
+			if(!empty($rp_init) && $match){
+				$queries;
+				parse_str($_SERVER["QUERY_STRING"],$queries);
+				add_action('admin_head',array($this,"init_script"));
+				add_action('admin_head',array($this,"confirm_script"));
+				delete_option("SS_RP_INIT");
+			}
+			if(empty($rp_confirm)){
+				//if($_GET["ssConfirmScript"] == 1){
+				//	update_option("SS_RP_CONFIRM",1);
+				//	echo "confirmed";
+				//	exit;
+				//}
+				//
+			}
+			if($_GET["ssDebugInfo"]){
+				$this->echo_debug_info();
+			}
 			$plugin = plugin_basename( __FILE__);
 			add_filter('plugin_action_links_' . $plugin, array($this, 'add_action_link') );
 			add_action('admin_menu', array($this, 'add_menu'));
-		}		
-		add_filter('the_content', array($this,'add_content_end'));
-		add_action('rss2_item', array($this,'add_post_thumbnail'));
+		}else{		
+			add_filter('request', array($this, 'filter_request'), 1 );
+			add_filter('the_content', array($this,'add_content_end'));
+			add_action('rss2_item', array($this,'add_post_thumbnail'));
+		}
 		if(function_exists("add_shortcode")){
-			add_shortcode('milliard',array($this,'add_content_end'));
+			add_shortcode('milliard',array($this,'echo_shortcode'));
 		}
 	}
+	public function echo_debug_info(){
 
+		$load_paths = array("wp-settings.php","wp-admin/admin.php","wp-admin/includes/admin.php",WPINC."/pluggable.php");
+		foreach($load_paths as $load_path){
+			require_once(ABSPATH.$load_path);
+		}
+		if ( ! current_user_can('activate_plugins') ){
+			wp_die( __( 'You do not have sufficient permissions to manage plugins for this site.' ) );
+		}
+		$items;
+		if(function_exists("_get_list_table")){
+			$_REQUEST["plugin_status"] = "active";
+			$wp_list_table = _get_list_table('WP_Plugins_List_Table');
+			$wp_list_table->prepare_items();
+			$items = $wp_list_table->items;
+		}else{ 
+			$items = get_plugins();
+		}
+		$home_url_str = (function_exists("home_url")) ? home_url() : get_bloginfo( 'url' );
+		echo "===SITEURL===\n";
+		echo $home_url_str."/\n";
+		echo "\n===PLUGIN===\n";
+		foreach($items as $item){
+			echo $item["Name"].",".$item["Version"]."\n";
+		}
+		echo "\n===PHPINFO===\n";
+		echo "v:".phpversion()."\n";
+		exit;
+	}
+	public function init_script(){
+		delete_option("SS_RP_INIT");
+		$script = $this->gen_script(); 
+		echo $script; 
+	}
+	public function confirm_script(){
+		$home_url_str = (function_exists("home_url")) ? home_url() : get_bloginfo( 'url' );
+		$script = '<script type="text/javascript">//<![CDATA[
+			window.Shisuh = (window.Shisuh) ? window.Shisuh : {};Shisuh.topUrl="'.$home_url_str.'/";Shisuh.type="Wordpress";
+		//]]>
+		</script><script id="ssConfirmRelatedPageScript" type="text/javascript" src="https://'.$this->host.'/djs/confirmRelatedPageScript/?requireLoader=1"></script>';
+
+		echo $script;
+	}
 	public function rss_url($link){
 		global $post;
 		$link = get_permalink($post->ID);
@@ -40,37 +102,40 @@ class ShisuhRelatedPage {
 		return $links;
 	}
 	public function add_content_end($content){
-		if(!is_feed() && !is_home() && is_single() && empty($this->isCalled)) {
-			$this->isCalled = true;
-			$alg = get_option("SS_RP_ALG");
-			if(!$alg){
-				$alg = "Related"; 
-			}
-			$show_bottom = get_option("SS_RP_SHOW_BOTTOM");
-			if(empty($show_bottom)){
-				$show_bottom = "1";
-			}
-			$show_insert = get_option("SS_RP_SHOW_INSERT"); 
-			if(empty($show_insert)){
-				$show_insert = "1";
-			}
-			$htOpt = get_option("SS_RP_HEADER_TEXT"); 
-			$headerText = (empty($htOpt)) ? "" : 'Shisuh.headerText = \''.$htOpt.'\';';
-			$fOpt = get_option("SS_RP_FOOTER_TEXT_COLOR");
-			$footerTextColor = (empty($fOpt)) ? "" :'Shisuh.footerTextColor=\''.$fOpt.'\';';
-			$home_url_str = (function_exists("home_url")) ? home_url() : get_bloginfo( 'url' );
-			$script = '<script type="text/javascript">//<![CDATA[
-				window.Shisuh = (window.Shisuh) ? window.Shisuh : {};Shisuh.topUrl="'.$home_url_str.'/";Shisuh.type="Wordpress";Shisuh.alg="'.$alg.'";Shisuh.showBottom="'.$show_bottom.'";Shisuh.showInsert="'.$show_insert.'";'.$headerText.$footerTextColor.'
-			//]]>
-			</script><script id="ssRelatedPageSdk" type="text/javascript" src="https://'.$this->host.'/djs/relatedPageFeed/"></script>';
+		if(!is_feed() && !is_home() && is_single()) {
+			$script = $this->gen_script(); 
 			$content .= $script;
 		}
 		return $content;
 	}
+	public function gen_script(){
+		$alg = get_option("SS_RP_ALG");
+		if(!$alg){
+			$alg = "Related"; 
+		}
+		$show_bottom = get_option("SS_RP_SHOW_BOTTOM");
+		if(empty($show_bottom)){
+			$show_bottom = "1";
+		}
+		$show_insert = get_option("SS_RP_SHOW_INSERT"); 
+		if(empty($show_insert)){
+			$show_insert = "1";
+		}
+		$htOpt = get_option("SS_RP_HEADER_TEXT"); 
+		$headerText = (empty($htOpt)) ? "" : 'Shisuh.headerText = \''.$htOpt.'\';';
+		$fOpt = get_option("SS_RP_FOOTER_TEXT_COLOR");
+		$footerTextColor = (empty($fOpt)) ? "" :'Shisuh.footerTextColor=\''.$fOpt.'\';';
+		$home_url_str = (function_exists("home_url")) ? home_url() : get_bloginfo( 'url' );
+		$script = '<script type="text/javascript">//<![CDATA[
+			window.Shisuh = (window.Shisuh) ? window.Shisuh : {};Shisuh.topUrl="'.$home_url_str.'/";Shisuh.type="Wordpress";Shisuh.alg="'.$alg.'";Shisuh.showBottom="'.$show_bottom.'";Shisuh.showInsert="'.$show_insert.'";'.$headerText.$footerTextColor.'
+		//]]>
+		</script><script id="ssRelatedPageSdk" type="text/javascript" src="https://'.$this->host.'/djs/relatedPageFeed/"></script>';
+		return $script;
+	}
 	public function add_menu() {
 		add_options_page(
-			'シスウ関連ページ設定画面', 
-			'シスウ関連ページ', 
+			'Milliard関連ページ設定画面', 
+			'Milliard関連ページ', 
 			8, 
 			__FILE__, 
 			array($this, 'options_page') 
@@ -80,20 +145,20 @@ class ShisuhRelatedPage {
 		$p = SS_RP_PLUGIN_DIR."/includes/admin_setting.php"; 
 		include_once($p); 
 	}
-	public function template($template){
+	/*public function template($template){
 		global $request;
 		if (ShisuhRelatedPage::isFeed($request)) {
-			return '';
+			return $template;
 		}else{
 			return $template;
 		}
-	}
+	}*/
 	public static function isFeed($request){
-		return (isset($request['feed']) && $request['feed'] == 'shisuhRelatedPage'); 
+		return ((isset($request['feed']) && $request['feed'] == 'shisuhRelatedPage') || (isset($_GET['shisuhRelatedPage']) && $_GET['shisuhRelatedPage'] == '1')); 
 	}
 	public function load_template_index(){
 		do_feed_rss2(false);
-
+		exit;
 	}
 	public function noLimit($limits){
 		$startIndex= $_GET["startIndex"];
@@ -114,11 +179,12 @@ class ShisuhRelatedPage {
 	public function filter_request($request){
 		$this->request = $request;
 		if (ShisuhRelatedPage::isFeed($request)) {
+			$request["feed"] = "";
 			add_action( 'pre_get_posts',array($this,'filter_feed'));
 			add_action( 'send_headers',array($this, 'send_nocache'));
-			add_action('do_feed_shisuhRelatedPage', array($this, 'load_template_index'), 10, 1);
 			add_action('post_limits', array($this, 'noLimit'),10,2);
 			add_filter('the_permalink_rss',array($this,'rss_url'),10000,10000);
+			add_action('template_redirect', array($this, 'load_template_index'), 10, 1);
 		}else{
 		}
 		return $request;
@@ -138,6 +204,18 @@ class ShisuhRelatedPage {
 			}
 		}
 	}
+	public function echo_shortcode(){
+		return '<ins id="ssRelatedPageBase"></ins>';
+	}
 } 
 
 $srp = & new ShisuhRelatedPage();
+function initSSRP(){
+	update_option("SS_RP_INIT",1);
+}
+function invalidateSSRP(){
+	delete_option("SS_RP_INIT");
+	delete_option("SS_RP_CONFIRM");
+}
+register_activation_hook(__FILE__,"initSSRP");
+register_deactivation_hook(__FILE__,"invalidateSSRP");
